0406503f85aea4e199e16e25d143257e995f4999
youngSlim01 <gyllromao@gmail.com>
2019-07-04 10:05:46 -0300

    versao 1

---
+++ application/controllers/Beneficiario.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') or exit ('No direct script access allowed');
+
+class Beneficiario extends CI_Controller
+{
+
+  public function index($value='')
+  {
+    $dados = array('titulo'=>'Beneficiarios');
+		$this->load->view('top',$dados);
+    $this->load->View("beneficiario/listar");
+		$this->load->view('botton');
+  }
+
+  public function cadastar($value='')
+  {
+    $dados = array('titulo'=>'Cadastar Beneficiario');
+		$this->load->view('top',$dados);
+    $this->load->View("beneficiario/cadastrar");
+		$this->load->view('botton');
+  }
+}
--- application/views/top.php
+++ application/views/top.php
@@ -1,21 +1,20 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+	defined('BASEPATH')OR exit('No direct script access allowed');
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<!-- start: Meta -->
 	<meta charset="utf-8">
 	<title>SisBeneficiario - GCR</title>
 	<meta name="description" content="Sistema de registo Beneficiario">
 	<meta name="author" content="GCR">
 	<meta name="keyword" content="GCR, Girl Child Right, GCR Dashboard, Sistema de Beneficiario, SisBeneficiario">
 	<!-- end: Meta -->
-	
+
 	<!-- start: Mobile Specific -->
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<!-- end: Mobile Specific -->
-	
+
 	<!-- start: CSS -->
 	<link id="bootstrap-style" href="<?php echo base_url();?>rg_assets/css/bootstrap.min.css" rel="stylesheet">
 	<link href="<?php echo base_url();?>rg_assets/css/bootstrap-responsive.min.css" rel="stylesheet">
@@ -23,18 +22,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<link id="base-style-responsive" href="<?php echo base_url();?>rg_assets/css/style-responsive.css" rel="stylesheet">
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800&subset=latin,cyrillic-ext,latin-ext' rel='stylesheet' type='text/css'>
 	<!-- end: CSS -->
-	
 
-	<!-- The HTML5 shim, for IE6-8 support of HTML5 elements -->
-	<!--[if lt IE 9]>
-	  	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-		<link id="ie-style" href="css/ie.css" rel="stylesheet">
-	<![endif]-->
-	
-	<!--[if IE 9]>
-		<link id="ie9style" href="css/ie9.css" rel="stylesheet">
-	<![endif]-->
-		
 	<!-- start: Favicon -->
 	<link rel="shortcut icon" href="img/favicon.ico">
 	<!-- end: Favicon -->
@@ -50,8 +38,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</a>
-				<a class="brand" href="#"><span>SisBeneficiario</span></a>
-								
+				<a class="brand" href="<?php echo base_url(''); ?>"><span>SisBeneficiario</span></a>
+
 				<!-- start: Header Menu -->
 				<div class="nav-no-collapse header-nav ">
 					<ul class="nav pull-right"
@@ -73,21 +61,21 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 					</ul>
 				</div>
 				<!-- end: Header Menu -->
-				
+
 			</div>
 		</div>
 	</div>
 	<!-- start: Header -->
-	
+
 		<div class="container-fluid-full">
 		<div class="row-fluid">
-				
+
 			<!-- start: Main Menu -->
 			<div id="sidebar-left" class="span2">
 				<div class="nav-collapse sidebar-nav">
 					<ul class="nav nav-tabs nav-stacked main-menu">
-						<li><a href="#"><i class="icon-dashboard"></i><span class="hidden-tablet"> Dashboard</span></a></li>	
-						<li><a href="#"><i class="icon-edit"></i><span class="hidden-tablet"> Beneficiarios</span></a></li>
+						<li><a href="<?php echo base_url(); ?>"><i class="icon-dashboard"></i><span class="hidden-tablet"> Dashboard</span></a></li>
+						<li><a href="<?php echo base_url('beneficiario'); ?>"><i class="icon-edit"></i><span class="hidden-tablet"> Beneficiarios</span></a></li>
 						<li><a href="#"><i class="icon-list-alt"></i><span class="hidden-tablet"> Graficos</span></a></li>
 						<li><a href="#"><i class="icon-tasks"></i><span class="hidden-tablet"> Relatorios</span></a></li>
 						<li>
@@ -95,28 +83,28 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 							<ul>
 								<li><a class="submenu" href=""><i class="icon-file-alt"></i><span class="hidden-tablet"> Projectos</span></a></li>
 								<li><a class="submenu" href=""><i class="icon-file-alt"></i><span class="hidden-tablet"> Servicos</span></a></li>
-							</ul>	
+							</ul>
 						</li>
 					</ul>
 				</div>
 			</div>
 			<!-- end: Main Menu -->
-			
+
 			<noscript>
 				<div class="alert alert-block span10">
 					<h4 class="alert-heading">Warning!</h4>
 					<p>You need to have <a href="http://en.wikipedia.org/wiki/JavaScript" target="_blank">JavaScript</a> enabled to use this site.</p>
 				</div>
 			</noscript>
-			
+
 			<!-- start: Content -->
 			<div id="content" class="span10">
-			
-			
+
+
 			<ul class="breadcrumb">
 				<li>
 					<i class="icon-home"></i>
-					<a href="index.html">Home</a> 
+					<a href="index.html">Home</a>
 					<i class="icon-angle-right"></i>
 				</li>
 				<li><a href="#"><?php echo $titulo?></a></li>
